f472c27bacfde92f515daf8f3ca1538106ee6754
MartinOscarMateo <martin.mateo@davinci.edu.ar>
2025-05-12 14:53:12 -0300

    commit de inicio

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import chalk from "chalk";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+import routerAPI from "./routes/index.js"
+
+app.use(express.json());
+
+app.get('/', (request, response) =>{
+    console.log('Ruta Raíz');
+    response.send('Home');
+});
+
+routerAPI(app);
+
+app.listen( port, () => {
+    console.log(   chalk.green(`Servidor Web en el puerto ${port}`) );    
+});
